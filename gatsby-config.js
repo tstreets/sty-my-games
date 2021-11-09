d412450f8beab864b1f757edfeb25a6bc252bc97
Ty Streets <tstreets@iu.edu>
2021-11-09 00:48:23 -0500

    Build #6; Added PWA functionality

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,15 @@ module.exports = {
     },
     plugins: [
         {
+            resolve: 'gatsby-plugin-manifest',
+            options: {
+                icon: 'src/images/logo.png',
+                short_name: `Streets Games`,
+                display: 'standalone',
+            },
+        },
+        'gatsby-plugin-offline',
+        {
             resolve: 'gatsby-plugin-firebase-v9.0',
             options: {
                 credentials: require('./firebase-cred.js'),
---
+++ public/component---cache-caches-gatsby-plugin-offline-app-shell-js-2642c0dbf5fe3b57ddf2.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunksty_my_games=self.webpackChunksty_my_games||[]).push([[306],{1930:function(e,t,n){var u=n(5318);t.__esModule=!0,t.default=void 0;var a=u(n(5354)),r=u(n(7294)),l=function(e){function t(){return e.apply(this,arguments)||this}return(0,a.default)(t,e),t.prototype.render=function(){return r.default.createElement(r.default.Fragment,null)},t}(r.default.Component),f=l;t.default=f}}]);
+//# sourceMappingURL=component---cache-caches-gatsby-plugin-offline-app-shell-js-2642c0dbf5fe3b57ddf2.js.map
